b2e6ba5cd27f819122fc873d22340443557bc548
Chonwai <chonwaiun@gmail.com>
2025-07-30 01:59:34 +0800

    fix: Fix the sidebar style of the DashboardLayout component, remove unnecessary CSS classes to improve readability

--- apps/frontend/src/components/DashboardLayout.tsx
+++ apps/frontend/src/components/DashboardLayout.tsx
@@ -89,7 +89,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
 
       {/* Sidebar */}
       <div
-        className={`fixed inset-y-0 left-0 z-50 w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:inset-0 ${
+        className={`fixed inset-y-0 left-0 z-50 w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out lg:translate-x-0 ${
           sidebarOpen ? 'translate-x-0' : '-translate-x-full'
         }`}
       >
